ee5f52ad60f6face00bb4160deed3e38a899866f
Adarsh Gupta <adarshgupta04022005@gmail.com>
2026-02-17 22:48:39 +0530

    Initial commit

---
+++ arrays/01_reverse_array.py
@@ -0,0 +1,26 @@
+"""
+Problem: Reverse an Array
+Platform: GeeksforGeeks
+Link: https://www.geeksforgeeks.org/problems/reverse-an-array/1
+
+Submission Details:
+Date: 2026-02-17 22:32:10
+Status: Correct
+Language: Python 3
+Test Cases Passed: 1115 / 1115
+
+"""
+
+class Solution:
+    def reverseArray(self, arr):
+        # Initialize two pointers
+        l = 0
+        r = len(arr) - 1 
+        
+        # Swap elements until pointers meet
+        while l < r:
+            arr[l], arr[r] = arr[r], arr[l]
+            l += 1 
+            r -= 1 
+            
+        return arr
